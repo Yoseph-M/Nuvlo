c24ffd53599cbabc22cdfbf30f67427cfcb27659
Yoseph-M <zubease22@gmail.com>
2026-05-21 23:52:47 +0300

    feat(models): add centralized model exports

---
+++ src/models/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Central model exports.
+ *
+ * Usage:
+ *   import { User, Property, Booking } from "@/models";
+ */
+
+export { User, type IUser, type UserRole } from "./User";
+export { Property, type IProperty, type IPropertyUtilities, type IPropertyLocation, type IPropertyCoordinates } from "./Property";
+export { Booking, type IBooking, type BookingStatus } from "./Booking";
+export { Payment, type IPayment, type PaymentProvider, type PaymentStatus } from "./Payment";
+export { Review, type IReview } from "./Review";
+export { Message, type IMessage } from "./Message";
